98e77aa3528efe9141c269b8f2d6f10d67cc7130
ksh5324 <yolly050101@naver.com>
2021-04-03 20:41:01 +0900

    test

---
+++ a.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var a = 1
+    var b = 2
+    var array = arrayOf<Int>(a, b)
+    for(i in array){
+        println(i)
+     }
+}
--- kotlin2.kt
+++ kotlin2.kt
@@ -1,8 +1,13 @@
 fun main(){
     var a: Int = 54321
     var b: Long = a.toLong()
-    var intArr = arrayOf(1, 2, 3, 4, 5)
+    var intArr = arrayListOf(1, 2, 3, 4, 5)  // 함수 사용 못함 변할수 없읍
     var nullrr = arrayOfNulls<Int>(5)
+    val mutableList = mutableListOf<Int>(1,3,5) // 함수 사용 가능
     intArr[2] = 8
     println(intArr[4])
+    for(i in intArr)
+    {
+        println(i)
+    }
 }
